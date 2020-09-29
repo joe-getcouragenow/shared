d24118320f2c4cab0be52940b1c326bba6212ee7
gutterbacon <gutterbacon@protonmail.com>
2020-09-29 05:21:00 +0000

    shared: copied tools from module repo

---
+++ tool/bs-box/maintemplate/go.mod
@@ -0,0 +1,3 @@
+module github.com/getcouragenow/packages/tool/bs-box/maintemplate
+
+go 1.13
---
+++ tool/bs-lang/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/getcouragenow/packages/tool/bs-lang/cmd"
+
+func main() {
+	cmd.Execute()
+}
